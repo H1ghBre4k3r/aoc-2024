721701901b824e808ab4520d160de0c0795b87cf
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2024-12-09 01:52:55 +0100

    feat: somehow not working day 6 part 2

--- src/day_06.rs
+++ src/day_06.rs
@@ -1,8 +1,8 @@
-use std::ops::{Add, AddAssign};
+use std::{collections::HashSet, ops::Add};
 
 use aoc_runner_derive::{aoc, aoc_generator};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Coord(i64, i64);
 
 impl Add for Coord {
@@ -10,13 +10,6 @@ impl Add for Coord {
 
     fn add(self, rhs: Self) -> Self::Output {
         Coord(self.0 + rhs.0, self.1 + rhs.1)
-    }
-}
-
-impl AddAssign for Coord {
-    fn add_assign(&mut self, rhs: Self) {
-        self.0 += rhs.0;
-        self.1 += rhs.1;
     }
 }
 
@@ -29,13 +22,19 @@ impl Map {
         y < (map.len() as i64) && y >= 0 && x < (map[0].len() as i64) && x >= 0
     }
 
-    fn is_wall(&self, Coord(x, y): Coord) -> bool {
-        let map = &self.0;
-        let Some(c) = map.get(y as usize).and_then(|map| map.get(x as usize)) else {
+    fn is_wall(&self, coord: Coord) -> bool {
+        let Some(c) = self.get(coord) else {
             return false;
         };
 
-        *c == '#'
+        c == '#'
+    }
+
+    fn get(&self, Coord(x, y): Coord) -> Option<char> {
+        self.0
+            .get(y as usize)
+            .and_then(|map| map.get(x as usize))
+            .cloned()
     }
 
     fn mark(&mut self, Coord(x, y): Coord) {
@@ -50,7 +49,6 @@ impl Map {
 #[derive(Debug, Clone, PartialEq, Eq)]
 struct Input {
     map: Map,
-    dim: (i64, i64),
     guard: Guard,
 }
 
@@ -60,7 +58,7 @@ struct Guard {
     direction: Direction,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Direction {
     Up,
     Down,
@@ -137,13 +135,12 @@ fn generator(input: &str) -> Input {
 
     Input {
         map: Map(map),
-        dim: (width as i64, height as i64),
         guard: guard.unwrap(),
     }
 }
 
 #[aoc(day6, part1)]
-fn part1(Input { map, dim, guard }: &Input) -> usize {
+fn part1(Input { map, guard, .. }: &Input) -> usize {
     let Guard {
         position,
         direction,
@@ -152,7 +149,7 @@ fn part1(Input { map, dim, guard }: &Input) -> usize {
     let mut map = map.clone();
 
     let mut position = *position;
-    let mut direction = direction.clone();
+    let mut direction = *direction;
 
     while map.contains(position) {
         map.mark(position);
@@ -161,7 +158,7 @@ fn part1(Input { map, dim, guard }: &Input) -> usize {
 
         if map.is_wall(new_position) {
             direction = direction.turn();
-            position += direction.offset();
+            position = position + direction.offset();
         } else {
             position = new_position;
         }
@@ -180,11 +177,72 @@ fn part1(Input { map, dim, guard }: &Input) -> usize {
     sum
 }
 
+#[aoc(day6, part2)]
+fn part2(Input { map, guard, .. }: &Input) -> usize {
+    let Guard {
+        position,
+        direction,
+    } = guard;
+    let start = *position;
+
+    let mut position = *position;
+    let mut direction = *direction;
+
+    let mut sum = 0;
+
+    let mut already_tested = HashSet::new();
+
+    while map.contains(position) {
+        let new_position = position + direction.offset();
+        if map.is_wall(new_position) {
+            direction = direction.turn();
+            continue;
+        }
+
+        // if there is no wall, we just place to obstacle right in front of us
+        let obstacle = new_position;
+
+        // only count each successful location once and do not test start
+        if !already_tested.contains(&obstacle) && obstacle != start && map.contains(obstacle) {
+            // track visited positions and directions
+            let mut visited = HashSet::new();
+
+            let mut sim_position = position;
+
+            // since we know that there is an obstacle, we just turn right
+            let mut sim_direction = direction.turn();
+
+            while map.contains(sim_position) {
+                // check, if we've already been here
+                if visited.contains(&(sim_position, sim_direction)) {
+                    already_tested.insert(obstacle);
+                    sum += 1;
+                    break;
+                }
+                visited.insert((sim_position, sim_direction));
+
+                // check for wall or our obstacle
+                let new_position = sim_position + sim_direction.offset();
+                if map.is_wall(new_position) || new_position == obstacle {
+                    sim_direction = sim_direction.turn();
+                    continue;
+                }
+
+                sim_position = new_position;
+            }
+        }
+
+        position = new_position;
+    }
+
+    sum
+}
+
 #[cfg(test)]
 mod tests {
     use crate::day_06::{Coord, Direction, Guard};
 
-    use super::{generator, part1};
+    use super::{generator, part1, part2};
 
     const INPUT: &str = "....#.....
 .........#
@@ -201,7 +259,6 @@ mod tests {
     fn test_generator() {
         let gen = generator(INPUT);
 
-        assert_eq!(gen.dim, (10, 10));
         assert_eq!(
             gen.guard,
             Guard {
@@ -218,5 +275,68 @@ mod tests {
         let res = part1(&gen);
 
         assert_eq!(res, 41);
+    }
+
+    #[test]
+    fn test_part2() {
+        let gen = generator(INPUT);
+
+        let res = part2(&gen);
+
+        assert_eq!(res, 6);
+    }
+
+    /*
+     * This can create three different loops:
+     *
+     * ...#0....
+     * ...++--+#
+     * .#.||..|.
+     * ...||.#|.
+     * ...||..|.
+     * ..#+---+.
+     * ....|..#.
+     * #...|....
+     * ....^##..
+     *
+     * ...#.....
+     * ........#
+     * .#..0....
+     * .+--++#..
+     * .|..||...
+     * .|#.||...
+     * .|..||.#.
+     * #+---+...
+     * ....^##..
+     *
+     * ...#.....
+     * ........#
+     * .#.......
+     * .+---+#..
+     * .|...|...
+     * .|#.0|...
+     * .|..+-+#.
+     * #+---++..
+     * ....^##..
+     */
+    const OTHER: &str = "...#.....
+........#
+.#.......
+......#..
+.........
+..#......
+.......#.
+#........
+....^##..";
+
+    #[test]
+    fn test_other() {
+        let gen = generator(OTHER);
+
+        let res = part1(&gen);
+        assert_eq!(res, 9);
+
+        let res = part2(&gen);
+        assert_eq!(res, 3);
     }
 }
